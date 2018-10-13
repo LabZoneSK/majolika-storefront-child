24428a89d2540ef7c4312e11161a2bcf12fbaa85
Martin Starosta <martin.starosta83@gmail.com>
2018-10-13 18:00:47 +0200

    Favicon + order No.

--- functions.php
+++ functions.php
@@ -26,3 +26,12 @@ add_action('wp_enqueue_scripts', 'majolika_child_styles', 999);
 
 /** Set image compression level */
 add_filter('jpeg_quality', function($arg) {return 100;});
+
+/** Add prefix to the Orders */
+add_filter( 'woocommerce_order_number', 'change_woocommerce_order_number' );
+
+function change_woocommerce_order_number( $order_id ) {
+$prefix = '2018';
+$new_order_id = $prefix . $order_id ;
+return $new_order_id;
+}
